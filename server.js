b2ab7ad9348e22763dc875bf6b91cb5fd8ed08c0
prince-konwea <kokodecontee123@gmail.com>
2022-10-22 10:15:28 +0100

    create nav section with join and sign in button

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 
-const initial_path = path.join(_dirname, "public");
+const initial_path = path.join(__dirname, "public");
 
 const app = express();
 
